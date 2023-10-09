83b00be1c7d77a3d9a162ecd41085e178c40bd17
Stefanus Adyan <stefanusadyan@gmail.com>
2023-10-09 17:35:01 +0700

    Initial commit

---
+++ BootcampBatch6/ClassCat/Cat.cs
@@ -0,0 +1,11 @@
+namespace Animal{
+	class Cat
+	{
+		public string colour;
+		
+		public void jump()
+		{
+			Console.WriteLine("Jump");
+		}
+	}
+}
---
+++ BootcampBatch6/ClassCat/Program.cs
@@ -0,0 +1,10 @@
+﻿
+class Program
+{
+	static void Main (string[] args)
+	{
+		Cat oyen = new Cat();
+		oyen.colour = "orange";
+		oyen.jump();
+	}
+}
